42ff20da1047d0fe7618a8ab55ce160586848ade
TuPM172885 <tuphamads@gmail.com>
2021-05-25 16:54:07 +0700

    first commit

---
+++ BTL_OS/cmake-build-debug/CMakeFiles/System_Operation_Project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/System_Operation_Project.dir/Caltrain-runner.c.o"
+  "System_Operation_Project"
+  "System_Operation_Project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/System_Operation_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
